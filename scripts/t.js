901f2bbe3c17f1a7c076091def86ca00cb3af295
Tewell <tewell.gewargis@generalassemb.ly>
2016-05-19 23:24:02 -0700

    trying wildcard

--- scripts/t.js
+++ scripts/t.js
@@ -5,10 +5,19 @@ module.exports = function (juneBot) {
   })
 
 
-  juneBot.respond((/hi/i) || (/hey/i), function(hiRes) {
+/*  juneBot.respond((/hi/i) || (/hey/i), function(hiRes) {
     if(hiRes == "hi") {
       return hiRes.reply("Well, hi to you too, good lookin' ;)")
     } else {
+      return hiRes.reply("Hey, how you doin'?")
+    }
+  })*/
+
+  junBot.respond(/(.*)/i, function(hiRes){
+    var resType = res.match[1]
+    if (resType === "hi") {
+      return hiRes.reply("Well, hi to you too, good lookin' ;)")
+    } else if (resType === "hey") {
       return hiRes.reply("Hey, how you doin'?")
     }
   })
